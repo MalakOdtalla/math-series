871761d4fb7cf613a30639666cf682e3e50089f8
Malak Odtalla <malakodt35@gmail.com>
2022-06-09 21:05:00 +0300

    add changes

--- series/series.py
+++ series/series.py
@@ -7,6 +7,7 @@ def fibonacci(n):
     else:
         return (fibonacci(n - 1) + fibonacci(n - 2))
 
+
 def lucas(n):
     ''' Fun takes one parameter n, Then  return the nth value in the lucas serie that start with(integers 2 and 1), using recursion  '''
     if (n == 0):
@@ -18,22 +19,18 @@ def lucas(n):
     return lucas(n - 1) + lucas(n - 2)
 
 
-def sum_series (nth,num0=0,num1=1):
-    if nth ==True:
-        return fibonacci(nth)
-    elif (num0==2 and num1==1):
-        return lucas(nth)
-    else:
-        return
+def sum_series (n,num0=0,num1=1):
+    ''' func with one required params that will determine which element in the series to print '''
+    for i in range(n):
+        num0, num1 = num1, num0 + num1
+    return num0
 
 
 
 
-#print(sum_series (7))
 
-
-
-
-#if __name__ == "__main__":
- #     input=input("Enter num :    ")
-  #    print(fibonacci(int(input)))
+# if __name__ == "__main__":
+#       input=input("Enter num :    ")
+#       print(fibonacci(int(input)))
+#       print(lucas(int(input)))
+#       print(sum_series(int(input)))
--- test_series/test_series.py
+++ test_series/test_series.py
@@ -1,17 +1,35 @@
-from series.series import fibonacci
-from series.series import lucas
+from series.series import fibonacci,lucas ,sum_series
 import pytest
 
 
-def fibonacci_test1():
+# fibonacci tests
+def test_fibonacci():
     actual=fibonacci(9)
     expected=34
 
-    assert_actual == expected
+    assert actual == expected
 
-
-def lucas_test1():
+# lucas tests
+def test_lucas():
     actual = lucas(9)
     expected=76
 
-    assert_actual == expected
+    assert actual == expected
+
+# sum_series tests
+
+def test_sum_series1():
+    actual = sum_series (0)
+    expected = 0
+    assert actual == expected
+
+def test_sum_series2():
+    actual = sum_series(0, 0, 1)
+    expected = 0
+    assert actual == expected
+
+
+def test_sum_series3():
+    actual = sum_series(0, 2, 1)
+    expected = 2
+    assert actual == expected
